e1e7e93c4b7e1883bf5ca196e4a01073aa6319a8
kmsk99 <momoqq@naver.com>
2023-12-04 00:01:20 +0900

    Update Atari QNet script with new features

--- atari_qnet.py
+++ atari_qnet.py
@@ -1,4 +1,6 @@
 # 라이브러리 임포트
+import glob
+import os
 import gymnasium as gym
 import collections
 import random
@@ -7,6 +9,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
 
 # CUDA 사용 가능 여부 확인
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -14,8 +17,34 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # 하이퍼파라미터 설정
 learning_rate = 0.0005  # 학습률
 gamma = 0.98  # 할인계수
-buffer_limit = 10000  # 버퍼 크기를 줄임
-batch_size = 16  # 배치 크기를 줄임
+buffer_limit = 50000  # 버퍼 크기
+batch_size = 32  # 배치 크기
+
+# 체크포인트 저장 폴더 생성
+checkpoint_dir = "checkpoints"
+os.makedirs(checkpoint_dir, exist_ok=True)
+
+# 체크포인트 저장 함수
+def save_checkpoint(state, episode, scores, checkpoint_dir=checkpoint_dir, keep_last=2):
+    filepath = os.path.join(checkpoint_dir, f"checkpoint_{episode}.pth")
+    torch.save({
+        'state': state,
+        'scores': scores
+    }, filepath)
+
+    # 오래된 체크포인트 삭제
+    checkpoints = sorted(glob.glob(os.path.join(checkpoint_dir, "checkpoint_*.pth")))
+    if len(checkpoints) > keep_last:
+        os.remove(checkpoints[0])  # 가장 오래된 체크포인트 삭제
+        
+# 체크포인트 로드 함수
+def load_checkpoint(checkpoint_dir=checkpoint_dir):
+    checkpoints = sorted(glob.glob(os.path.join(checkpoint_dir, "checkpoint_*.pth")))
+    if not checkpoints:
+        return None, None  # 체크포인트가 없을 경우 None 반환
+    latest_checkpoint = checkpoints[-1]  # 가장 최근의 체크포인트
+    checkpoint = torch.load(latest_checkpoint)
+    return checkpoint['state'], checkpoint['scores']
 
 # 경험 리플레이 버퍼 정의
 class ReplayBuffer:
@@ -123,6 +152,26 @@ def train(q, q_target, memory, optimizer):
         loss.backward()
         optimizer.step()
 
+# 그래프 그리기 및 저장 함수 업데이트
+def plot_scores(scores, filename):
+    plt.figure(figsize=(12, 6))
+    plt.plot(scores, label='Score', color='blue')
+    
+    # 10개 이동평균 계산
+    moving_avg_10 = [np.mean(scores[max(0, i-9):i+1]) for i in range(len(scores))]
+    plt.plot(moving_avg_10, label='10-episode Moving Avg', color='green')
+    
+    # 50개 이동평균 계산
+    moving_avg_50 = [np.mean(scores[max(0, i-49):i+1]) for i in range(len(scores))]
+    plt.plot(moving_avg_50, label='50-episode Moving Avg', color='red')
+    
+    plt.xlabel('Episode')
+    plt.ylabel('Score')
+    plt.title('Scores per Episode')
+    plt.legend()
+    plt.savefig(filename)
+    plt.close()
+
 # 메인 함수 정의
 def main():
     env = gym.make("ALE/SpaceInvaders-v5")  # 환경 초기화
@@ -132,13 +181,29 @@ def main():
     q_target.load_state_dict(q.state_dict())
     memory = ReplayBuffer()
 
-    print_interval = 5
+    print_interval = 10
     score = 0.0
+    average_score = 0.0
     optimizer = optim.Adam(q.parameters(), lr=learning_rate)
+    
+    scores = []  # 에피소드별 점수 저장 리스트
 
-    for n_epi in range(1000):
+    # 체크포인트 로드 (필요한 경우)
+    checkpoint, loaded_scores = load_checkpoint()
+    start_episode = 0  # 시작 에피소드 번호 초기화
+    if checkpoint:
+        q.load_state_dict(checkpoint['model_state'])
+        q_target.load_state_dict(checkpoint['target_model_state'])
+        optimizer.load_state_dict(checkpoint['optimizer_state'])
+        memory = checkpoint['memory']
+        start_episode = checkpoint['episode']
+        scores = loaded_scores  # 이전 점수 이력 로드
+
+    checkpoint_interval = 100  # 체크포인트 저장 간격
+
+    for n_epi in range(start_episode, 3000):
         epsilon = max(
-            0.01, 0.08 - 0.01 * (n_epi / 200)  # 탐험률 조정
+            0.005, 0.08 - 0.01 * (n_epi / 200)  # 탐험률 조정
         )
         s, _ = env.reset()
         done = False
@@ -152,6 +217,7 @@ def main():
             s = s_prime
 
             score += r
+            average_score += r
             if done:
                 break
 
@@ -162,10 +228,27 @@ def main():
             q_target.load_state_dict(q.state_dict())
             print(
                 "n_episode :{}, score : {:.1f}, n_buffer : {}, eps : {:.1f}%".format(
-                    n_epi, score / print_interval, memory.size(), epsilon * 100
+                    n_epi, average_score / print_interval, memory.size(), epsilon * 100
                 )
             )
-            score = 0.0
+            average_score = 0.0
+            
+        scores.append(score)  # 점수 저장
+        score = 0
+
+        # 매 10번째 에피소드마다 그래프 업데이트 및 저장
+        if n_epi % 10 == 0:
+            plot_scores(scores, 'score_plot.png')
+
+        # 체크포인트 저장
+        if n_epi % checkpoint_interval == 0 and n_epi != 0:
+            save_checkpoint({
+                'model_state': q.state_dict(),
+                'target_model_state': q_target.state_dict(),
+                'optimizer_state': optimizer.state_dict(),
+                'memory': memory,
+                'episode': n_epi  # 현재 에피소드 번호 저장
+            }, n_epi, scores)
 
     env.close()
 
