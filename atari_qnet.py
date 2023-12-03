eb1df456966296ce1bab72356f1c35fd79785b90
kmsk99 <momoqq@naver.com>
2023-12-03 22:48:12 +0900

    주석 추가

--- atari_qnet.py
+++ atari_qnet.py
@@ -1,97 +1,98 @@
-# libraries
+# 라이브러리 임포트
 import gymnasium as gym
 import collections
 import random
 import numpy as np
-
-# pytorch library is used for deep learning
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 
-# hyperparameters
-learning_rate = 0.0005
-gamma = 0.98
-buffer_limit = 50000  # size of replay buffer
-batch_size = 32
+# 하이퍼파라미터 설정
+learning_rate = 0.0005  # 학습률
+gamma = 0.98  # 할인계수
+buffer_limit = 50000  # 경험 리플레이 버퍼의 최대 크기
+batch_size = 32  # 배치 크기
 
-
+# 경험 리플레이 버퍼 정의
 class ReplayBuffer:
     def __init__(self):
-        self.buffer = collections.deque(maxlen=buffer_limit)  # double-ended queue
+        self.buffer = collections.deque(maxlen=buffer_limit)  # 더블 엔드 큐를 사용한 버퍼 초기화
 
     def put(self, transition):
-        self.buffer.append(transition)
+        self.buffer.append(transition)  # 버퍼에 경험 추가
 
     def sample(self, n):
-        mini_batch = random.sample(self.buffer, n)
+        mini_batch = random.sample(self.buffer, n)  # 버퍼에서 무작위로 n개 샘플 추출
         s_lst, a_lst, r_lst, s_prime_lst, done_mask_lst = [], [], [], [], []
 
         for transition in mini_batch:
             s, a, r, s_prime, done_mask = transition
-            s_lst.append(s.transpose(2, 0, 1))
+            s_lst.append(s.transpose(2, 0, 1))  # 차원 순서 변경
             s_prime_lst.append(s_prime.transpose(2, 0, 1))
             a_lst.append([a])
-            r_lst.append([r])  # 보상을 리스트에 추가
+            r_lst.append([r])
             done_mask_lst.append([done_mask])
 
+        # numpy 배열로 변환한 후 torch 텐서로 변환
         return (
             torch.tensor(np.array(s_lst), dtype=torch.float),
             torch.tensor(np.array(a_lst), dtype=torch.long),
-            torch.tensor(np.array(r_lst), dtype=torch.float),  # 보상 텐서의 데이터 타입을 Float으로 지정
+            torch.tensor(np.array(r_lst), dtype=torch.float),
             torch.tensor(np.array(s_prime_lst), dtype=torch.float),
             torch.tensor(np.array(done_mask_lst)),
         )
 
     def size(self):
-        return len(self.buffer)
+        return len(self.buffer)  # 버퍼 크기 반환
 
-
+# Q-네트워크 정의
 class Qnet(nn.Module):
     def __init__(self):
         super(Qnet, self).__init__()
-        # CNN 레이어
+        # CNN 레이어 정의
         self.conv1 = nn.Conv2d(3, 32, kernel_size=8, stride=4)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
         self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
 
-        # 완전 연결 레이어
+        # 완전 연결 레이어 정의
         self.fc1 = nn.Linear(self._feature_size(), 512)
-        self.fc2 = nn.Linear(512, 6)  # 6개 액션
+        self.fc2 = nn.Linear(512, 6)  # 액션의 개수에 따라 출력 크기 설정
 
     def _feature_size(self):
-        # 임시 데이터를 통해 CNN 출력 크기 계산
+        # CNN 출력 크기 계산을 위한 임시 데이터 처리
         with torch.no_grad():
             return nn.Sequential(self.conv1, self.conv2, self.conv3).forward(torch.zeros(1, 3, 210, 160)).view(1, -1).size(1)
 
     def forward(self, x):
+        # 신경망 순전파 정의
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
         x = F.relu(self.conv3(x))
-        x = x.reshape(x.size(0), -1)  # 평탄화: 'view' 대신 'reshape' 사용
+        x = x.reshape(x.size(0), -1)  # 데이터 평탄화
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
 
     def sample_action(self, obs, epsilon):
+        # 행동 선택 메서드
         if not isinstance(obs, torch.Tensor):
             obs = torch.from_numpy(obs).float()  # NumPy 배열을 Tensor로 변환
-        obs = obs.unsqueeze(0)  # 단일 관찰에 배치 차원 추가
+        obs = obs.unsqueeze(0)  # 배치 차원 추가
         obs = obs.permute(0, 3, 1, 2)  # 차원 재정렬
         out = self.forward(obs)
         coin = random.random()
         if coin < epsilon:
-            return random.randint(0, 5)  # 무작위 액션
+            return random.randint(0, 5)  # 무작위 액션 선택
         else:
-            return out.argmax().item()
+            return out.argmax().item()  # 최적의 액션 선택
 
-
+# 훈련 함수 정의
 def train(q, q_target, memory, optimizer):
     for i in range(10):
         s, a, r, s_prime, done_mask = memory.sample(batch_size)
 
-        # Ensure tensors are of type Float
+        # 텐서 타입을 Float로 설정
         s = s.float()
         s_prime = s_prime.float()
         r = r.float()
@@ -100,23 +101,21 @@ def train(q, q_target, memory, optimizer):
         q_out = q(s)
         q_a = q_out.gather(1, a)
 
-        # Double DQN
+        # Double DQN 구현
         argmax_Q = q(s_prime).max(1)[1].unsqueeze(1)
         max_q_prime = q_target(s_prime).gather(1, argmax_Q)
 
         target = r + gamma * max_q_prime * done_mask
 
-        # MSE Loss
+        # 손실 함수 계산 및 역전파
         loss = F.mse_loss(q_a, target)
-
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-
-
+# 메인 함수 정의
 def main():
-    env = gym.make("ALE/SpaceInvaders-v5")
+    env = gym.make("ALE/SpaceInvaders-v5")  # 환경 초기화
     q = Qnet()
     q_target = Qnet()
     q_target.load_state_dict(q.state_dict())
@@ -128,8 +127,8 @@ def main():
 
     for n_epi in range(1000):
         epsilon = max(
-            0.01, 0.08 - 0.01 * (n_epi / 200)
-        )  # Linear annealing from 8% to 1%
+            0.01, 0.08 - 0.01 * (n_epi / 200)  # 탐험률 조정
+        )
         s, _ = env.reset()
         done = False
 
@@ -158,7 +157,6 @@ def main():
             score = 0.0
 
     env.close()
-
 
 if __name__ == "__main__":
     main()
