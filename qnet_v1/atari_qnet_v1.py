496b5497f857a98345523472435f6927208feef1
kmsk99 <momoqq@naver.com>
2023-12-04 22:54:05 +0900

    file moved

--- qnet_v1/atari_qnet_v1.py
+++ qnet_v1/atari_qnet_v1.py
@@ -21,22 +21,21 @@ buffer_limit = 50000  # 버퍼 크기
 batch_size = 32  # 배치 크기
 
 # 체크포인트 저장 폴더 생성
-checkpoint_dir = "qnet_v1_checkpoints"
+checkpoint_dir = "checkpoints"
 os.makedirs(checkpoint_dir, exist_ok=True)
+
 
 # 체크포인트 저장 함수
 def save_checkpoint(state, episode, scores, checkpoint_dir=checkpoint_dir, keep_last=2):
     filepath = os.path.join(checkpoint_dir, f"checkpoint_{episode}.pth")
-    torch.save({
-        'state': state,
-        'scores': scores
-    }, filepath)
+    torch.save({"state": state, "scores": scores}, filepath)
 
     # 오래된 체크포인트 삭제
     checkpoints = sorted(glob.glob(os.path.join(checkpoint_dir, "checkpoint_*.pth")))
     if len(checkpoints) > keep_last:
         os.remove(checkpoints[0])  # 가장 오래된 체크포인트 삭제
-        
+
+
 # 체크포인트 로드 함수
 def load_checkpoint(checkpoint_dir=checkpoint_dir):
     checkpoints = sorted(glob.glob(os.path.join(checkpoint_dir, "checkpoint_*.pth")))
@@ -44,7 +43,8 @@ def load_checkpoint(checkpoint_dir=checkpoint_dir):
         return None, None  # 체크포인트가 없을 경우 None 반환
     latest_checkpoint = checkpoints[-1]  # 가장 최근의 체크포인트
     checkpoint = torch.load(latest_checkpoint)
-    return checkpoint['state'], checkpoint['scores']
+    return checkpoint["state"], checkpoint["scores"]
+
 
 # 경험 리플레이 버퍼 정의
 class ReplayBuffer:
@@ -78,6 +78,7 @@ class ReplayBuffer:
     def size(self):
         return len(self.buffer)  # 버퍼 크기 반환
 
+
 # Q-네트워크 정의
 class Qnet(nn.Module):
     def __init__(self):
@@ -94,7 +95,12 @@ class Qnet(nn.Module):
     def _feature_size(self):
         # CNN 출력 크기 계산을 위한 임시 데이터 처리
         with torch.no_grad():
-            return nn.Sequential(self.conv1, self.conv2, self.conv3).forward(torch.zeros(1, 3, 210, 160)).view(1, -1).size(1)
+            return (
+                nn.Sequential(self.conv1, self.conv2, self.conv3)
+                .forward(torch.zeros(1, 3, 210, 160))
+                .view(1, -1)
+                .size(1)
+            )
 
     def forward(self, x):
         # 신경망 순전파 정의
@@ -118,6 +124,7 @@ class Qnet(nn.Module):
             return random.randint(0, 5)  # 무작위 액션 선택
         else:
             return out.argmax().item()  # 최적의 액션 선택
+
 
 # 훈련 함수 정의
 def train(q, q_target, memory, optimizer):
@@ -152,31 +159,36 @@ def train(q, q_target, memory, optimizer):
         loss.backward()
         optimizer.step()
 
+
 # 그래프 그리기 및 저장 함수 업데이트
 def plot_scores(scores, filename):
     plt.figure(figsize=(12, 6))
-    plt.plot(scores, label='Score', color='blue')
-    
+    plt.plot(scores, label="Score", color="blue")
+
     # 10개 이동평균 계산
-    moving_avg_10 = [np.mean(scores[max(0, i-9):i+1]) for i in range(len(scores))]
-    plt.plot(moving_avg_10, label='10-episode Moving Avg', color='green')
-    
+    moving_avg_10 = [np.mean(scores[max(0, i - 9) : i + 1]) for i in range(len(scores))]
+    plt.plot(moving_avg_10, label="10-episode Moving Avg", color="green")
+
     # 50개 이동평균 계산
-    moving_avg_50 = [np.mean(scores[max(0, i-49):i+1]) for i in range(len(scores))]
-    plt.plot(moving_avg_50, label='50-episode Moving Avg', color='red')
-    
-    plt.xlabel('Episode')
-    plt.ylabel('Score')
-    plt.title('Scores per Episode')
+    moving_avg_50 = [
+        np.mean(scores[max(0, i - 49) : i + 1]) for i in range(len(scores))
+    ]
+    plt.plot(moving_avg_50, label="50-episode Moving Avg", color="red")
+
+    plt.xlabel("Episode")
+    plt.ylabel("Score")
+    plt.title("Scores per Episode")
     plt.legend()
     plt.savefig(filename)
     plt.close()
 
+
 # 메인 함수 정의
 def main():
-    env = gym.make("ALE/SpaceInvaders-v5"
-                #    , render_mode="human"
-                   )  # 환경 초기화
+    env = gym.make(
+        "ALE/SpaceInvaders-v5"
+        #    , render_mode="human"
+    )  # 환경 초기화
     # 모델을 GPU로 이동
     q = Qnet().to(device)
     q_target = Qnet().to(device)
@@ -187,25 +199,23 @@ def main():
     score = 0.0
     average_score = 0.0
     optimizer = optim.Adam(q.parameters(), lr=learning_rate)
-    
+
     scores = []  # 에피소드별 점수 저장 리스트
 
     # 체크포인트 로드 (필요한 경우)
     checkpoint, loaded_scores = load_checkpoint()
     start_episode = 0  # 시작 에피소드 번호 초기화
     if checkpoint:
-        q.load_state_dict(checkpoint['model_state'])
-        q_target.load_state_dict(checkpoint['target_model_state'])
-        optimizer.load_state_dict(checkpoint['optimizer_state'])
-        start_episode = checkpoint['episode']
+        q.load_state_dict(checkpoint["model_state"])
+        q_target.load_state_dict(checkpoint["target_model_state"])
+        optimizer.load_state_dict(checkpoint["optimizer_state"])
+        start_episode = checkpoint["episode"]
         scores = loaded_scores  # 이전 점수 이력 로드
 
     checkpoint_interval = 100  # 체크포인트 저장 간격
 
     for n_epi in range(start_episode, 3000):
-        epsilon = max(
-            0.005, 0.08 - 0.01 * (n_epi / 200)  # 탐험률 조정
-        )
+        epsilon = max(0.005, 0.08 - 0.01 * (n_epi / 200))  # 탐험률 조정
         s, _ = env.reset()
         done = False
 
@@ -233,24 +243,29 @@ def main():
                 )
             )
             average_score = 0.0
-            
+
         scores.append(score)  # 점수 저장
         score = 0
 
         # 매 10번째 에피소드마다 그래프 업데이트 및 저장
         if n_epi % 10 == 0:
-            plot_scores(scores, 'score_plot.png')
+            plot_scores(scores, "score_plot.png")
 
         # 체크포인트 저장
         if n_epi % checkpoint_interval == 0 and n_epi != 0:
-            save_checkpoint({
-                'model_state': q.state_dict(),
-                'target_model_state': q_target.state_dict(),
-                'optimizer_state': optimizer.state_dict(),
-                'episode': n_epi  # 현재 에피소드 번호 저장
-            }, n_epi, scores)
+            save_checkpoint(
+                {
+                    "model_state": q.state_dict(),
+                    "target_model_state": q_target.state_dict(),
+                    "optimizer_state": optimizer.state_dict(),
+                    "episode": n_epi,  # 현재 에피소드 번호 저장
+                },
+                n_epi,
+                scores,
+            )
 
     env.close()
+
 
 if __name__ == "__main__":
     main()
